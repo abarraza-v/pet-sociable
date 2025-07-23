8575cfa4c7f5c170d1a2d90133cdde8bb0bee942
Alejandro Barraza <abarraza.pk@gmail.com>
2025-07-22 23:33:10 -0400

    fix: Volviendo a corregir app.jsx y eliminando react.svg que sin querer se revertieron los cambios.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,7 @@ import PaginaPrincipal from "./pages/PaginaPrincipal";
 function App() {
   return (
     <>
-      <div
-      style={{
-        margin: 0,
-        backgroundImage: `url(${portada})`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundRepeat: 'no-repeat',
-        width: '100vw',
-        height: '100vh'
-      }}
-      >
-      </div>
+      <PaginaPrincipal/>
     </>
   );
 }
