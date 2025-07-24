c7cc3ff3e2b11da38077c969c06345be84d6dcfb
Alejandro Barraza <abarraza.pk@gmail.com>
2025-07-24 12:46:28 -0400

    feat(formulario-mascotas): implementar inputs controlados en formulario de registro
    - Se agregó estado local para los campos nombre, especie, raza, edad y descripción.
    - Se configuraron los inputs con value y onChange correspondientes.

--- src/components/mascota/FormularioMascotas.jsx
+++ src/components/mascota/FormularioMascotas.jsx
@@ -1,5 +1,14 @@
+import { useState } from "react";
+
 const FormularioMascotas = () => {
+    const [nombreMascota, setNombreMascota] = useState("");
+    const [especieMascota, setEspecieMascota] = useState("");
+    const [razaMascota, setRazaMascota] = useState("");
+    const [edadMascota, setEdadMascota] = useState("");
+    const [descripcionMascota, setDescripcionMascota] = useState("");
+
   const manejarEnvio = (evento) => {
+    console.log("Se clickeo el botón enviar");
     evento.preventDefault();
   };
 
@@ -7,12 +16,55 @@ const FormularioMascotas = () => {
     <>
       <h2>Registrar mascota</h2>
       <form onSubmit={manejarEnvio}>
-        <input type="text" placeholder="Nombre" name="nombre" />
-        <input type="text" placeholder="Especie" name="especie" />
-        <input type="text" placeholder="Raza" name="raza" />
-        <input type="number" placeholder="Edad" name="edad" />
-        <input type="text" placeholder="Nombre" name="nombre" />
-        <textarea placeholder="Descripción" name="descripcion"></textarea>
+        <div>
+          <label>Nombre:</label>
+          <input
+            type="text"
+            name="nombreMascota"
+            value={nombreMascota}
+            onChange={(e) => setNombreMascota(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Especie:</label>
+          <input
+            type="text"
+            name="especieMascota"
+            value={especieMascota}
+            onChange={(e) => setEspecieMascota(e.target.value)}
+          />
+        </div>
+
+         <div>
+          <label>Raza:</label>
+          <input
+            type="text"
+            name="razaMascota"
+            value={razaMascota}
+            onChange={(e) => setRazaMascota(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Edad:</label>
+          <input
+            type="number"
+            name="edadMascota"
+            value={edadMascota}
+            onChange={(e) => setEdadMascota(e.target.value)}
+          />
+        </div>
+
+        <div>
+          <label>Descripción:</label>
+          <textarea
+            name="descripcionMascota"
+            value={descripcionMascota}
+            onChange={(e) => setDescripcionMascota(e.target.value)}
+          />
+        </div>
+
         <button type="submit"> Enviar </button>
       </form>
     </>
